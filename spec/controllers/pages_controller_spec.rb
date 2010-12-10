94c6fa171f20789ecafdf507cd61dfa1b26f2670
Matt Diebolt <mdiebolt@gmail.com>
2010-12-09 17:52:29 -0800

    added title helper and blueprint

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -24,27 +28,30 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
   it "should have the right title" do
     get 'home'
-    response.should have_selector(
-      "title",
-      :content => "Ruby on Rails Tutorial Sample App | Home"
-    )
+    response.should have_selector("title", :content => @base_title + "Home")
   end
 
   it "should have the right title" do
     get 'contact'
-    response.should have_selector(
-      "title",
-      :content => "Ruby on Rails Tutorial Sample App | Contact"
-    )
+    response.should have_selector("title", :content => @base_title + "Contact")
   end
 
   it "should have the right title" do
     get 'about'
-    response.should have_selector(
-      "title",
-      :content => "Ruby on Rails Tutorial Sample App | About"
-    )
+    response.should have_selector("title", :content => @base_title + "About")
+  end
+
+  it "should have the right title" do
+    get 'help'
+    response.should have_selector("title", :content => @base_title + "Help")
   end
 end
