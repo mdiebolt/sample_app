f28d4628be2e8d9c905c9a57d82a2d16a6fab91b
Matt Diebolt <mdiebolt@gmail.com>
2010-12-10 09:02:25 -0800

    finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,12 +12,22 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + "Home")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => @base_title + "Contact")
     end
   end
 
@@ -26,6 +36,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => @base_title + "About")
+    end
   end
 
   describe "GET 'help'" do
@@ -33,25 +48,10 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
-  end
 
-  it "should have the right title" do
-    get 'home'
-    response.should have_selector("title", :content => @base_title + "Home")
-  end
-
-  it "should have the right title" do
-    get 'contact'
-    response.should have_selector("title", :content => @base_title + "Contact")
-  end
-
-  it "should have the right title" do
-    get 'about'
-    response.should have_selector("title", :content => @base_title + "About")
-  end
-
-  it "should have the right title" do
-    get 'help'
-    response.should have_selector("title", :content => @base_title + "Help")
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + "Help")
+    end
   end
 end
